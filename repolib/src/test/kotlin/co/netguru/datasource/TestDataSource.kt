dfbd4bb66c549b6547975d1bebc007364cdff06e
mmarkiew <maciej.markiew@gmail.com>
2018-11-22 15:55:09 +0100

    [task/RPI-7] DataSource logic for CRUD operations

---
+++ repolib/src/test/kotlin/co/netguru/datasource/TestDataSource.kt
@@ -0,0 +1,17 @@
+package co.netguru.datasource
+
+import co.netguru.TestDataEntity
+import io.reactivex.Completable
+import io.reactivex.Flowable
+
+class TestDataSource(private val data: List<TestDataEntity>) : DataSource<TestDataEntity>() {
+    override fun delete(query: Query<TestDataEntity>): Completable = Completable.complete()
+
+    override fun update(entity: TestDataEntity): Completable = Completable.complete()
+
+    override fun create(entity: TestDataEntity): Completable = Completable.complete()
+
+    public override fun query(request: Query<TestDataEntity>)
+            : Flowable<TestDataEntity> = Flowable.fromIterable(data)
+
+}
