f1c504452fdf44aeef42878eb8a5f8ddf11fbc30
mmarkiew <maciej.markiew@gmail.com>
2018-12-27 16:21:06 +0100

    [task/RPI-6] Dao update

--- sample/src/main/kotlin/co/netguru/repolib/feature/demo/datasource/localstore/DataDao.kt
+++ sample/src/main/kotlin/co/netguru/repolib/feature/demo/datasource/localstore/DataDao.kt
@@ -1,10 +1,8 @@
 package co.netguru.repolib.feature.demo.datasource.localstore
 
 import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
 
 open class DataDao : RealmObject() {
-    @PrimaryKey
     var id: Long? = null
     var value: String? = null
 }
