7101fb2250ec435a0e6481bf4cab700ac32c027c
mmarkiew <maciej.markiew@gmail.com>
2018-12-28 15:39:05 +0100

    [task/RPI-6] Create request mocking

--- sample/src/main/kotlin/co/netguru/repolib/feature/demo/datasource/api/API.kt
+++ sample/src/main/kotlin/co/netguru/repolib/feature/demo/datasource/api/API.kt
@@ -2,15 +2,22 @@ package co.netguru.repolib.feature.demo.datasource.api
 
 import io.reactivex.Completable
 import io.reactivex.Observable
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface API {
+
+    @DELETE("delete")
     fun delete(): Completable
 
     //    todo refactor
     @GET("getAll")
     fun get(): Observable<List<RemoteDataEntity>>
 
+    @POST("update")
     fun update(): Observable<RemoteDataEntity>
+
+    @POST("create")
     fun create(): Observable<RemoteDataEntity>
 }
--- sample/src/main/kotlin/co/netguru/repolib/feature/demo/datasource/api/MockingInterceptor.kt
+++ sample/src/main/kotlin/co/netguru/repolib/feature/demo/datasource/api/MockingInterceptor.kt
@@ -4,6 +4,11 @@ import okhttp3.*
 
 class MockingInterceptor : Interceptor {
 
+    private val singleElement = "{\n" +
+            "  \"id\": 1,\n" +
+            "  \"note\": \"new element note\"\n" +
+            "}"
+
     private val content = "[\n" +
             "{\n" +
             "  \"id\": 1,\n" +
@@ -24,16 +29,23 @@ class MockingInterceptor : Interceptor {
             "]" +
             ""
 
-    override fun intercept(chain: Interceptor.Chain): Response = selectResponse(chain.request().url())
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = Response.Builder()
+                .request(chain.request())
+                .message("ok")
+                .protocol(Protocol.HTTP_1_0)
 
-    private fun selectResponse(url: HttpUrl?): Response = when {
-        url.toString().contains("/getAll") -> Response.Builder()
-                .code(200)
+        return generateResponse(chain.request().url().toString(), builder).build()
+    }
+
+    private fun generateResponse(request: String, builder: Response.Builder): Response.Builder = when {
+        request.contains("/getAll") -> builder.code(200)
                 .body(ResponseBody.create(MediaType.parse("application/json"), content))
-                .build()
-        else -> Response.Builder()
-                .code(404)
-                .build()
+
+        request.contains("/create") -> builder.code(200)
+                .body(ResponseBody.create(MediaType.parse("application/json"), singleElement))
+        else -> builder.code(404)
+                .body(ResponseBody.create(MediaType.parse("application/json"), content))
     }
 
 
