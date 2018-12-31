d341d93801c0f0f9178357d89cc584a858f33e51
mmarkiew <maciej.markiew@gmail.com>
2018-12-31 10:08:12 +0100

    [task/RPI-6] Default Strategy revert

--- repolib/src/main/kotlin/co/netguru/repolibrx/strategy/DefaultRequestsStrategy.kt
+++ repolib/src/main/kotlin/co/netguru/repolibrx/strategy/DefaultRequestsStrategy.kt
@@ -7,11 +7,9 @@ class DefaultRequestsStrategy(
         private val requestsStrategy: RequestStrategy = RequestStrategy.LocalAfterUpdateOrFailureOfRemote
 ) : RequestsStrategy {
 
-    //todo refactor
-    override fun <T> select(request: Request<T>): Strategy = when (request.type) {
-        RequestType.CREATE -> RequestStrategy.OnlyRemote
-        RequestType.DELETE -> RequestStrategy.Both
-        RequestType.FETCH -> requestsStrategy
-        RequestType.UPDATE -> RequestStrategy.OnlyRemote
+    override fun <T> select(request: Request<T>): Strategy = if (request.type == RequestType.FETCH) {
+        requestsStrategy
+    } else {
+        RequestStrategy.OnlyRemote
     }
 }
